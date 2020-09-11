8d2e2792cf4eb47a3f16653335d388dfc645e9ff
FelipeHdez <felipinhdez@gmail.com>
2020-09-10 19:36:57 -0500

    Add basic classes and refactor board

--- main.rb
+++ main.rb
@@ -1,8 +1,28 @@
 # Stores the pegs
 class Board
+  attr_accessor :turns, :guesses, :code
   def initialize
-    @turns = Array.new(12, { code_pegs: Array.new(4, ''), key_pegs: Array.new(4, '') })
-    @guesses = Array.new(4, '')
-    @code = Array.new(4, '')
+    make_new_board
   end
+
+  def make_new_board
+    @turns = Array.new(12, { code_pegs: Array.new(4, 0), key_pegs: Array.new(4, 0) })
+    @code = Array.new(4, 0)
+  end
+end
+
+# Handles game logic
+class Game
+  def initialize
+    @board = Board.new
+    @players = [HumanPlayer.new, ComputerPlayer.new]
+  end
+end
+
+# Handles the computer logic
+class ComputerPlayer
+end
+
+# Handles user input
+class HumanPlayer
 end
